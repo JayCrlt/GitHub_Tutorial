d12773726bf3ff1f236fc646335fdbaaba7d6d6c
Jérémy T. Carlot <47741658+JayCrlt@users.noreply.github.com>
2023-03-15 11:33:40 +0100

    new_figure

--- R/my_amazing_script.R
+++ R/my_amazing_script.R
@@ -1,6 +1,11 @@
 library(ggplot2)
 my_amazing_data <- iris
-ggplot(my_amazing_data) + 
-  geom_point(aes(x = Sepal.Length, y = Sepal.Width, fill = Species), shape = 21) +
-  scale_fill_manual(values = c("red", "blue", "green")) +
-  theme_classic()
+my_colors <- c("brown3", "cornflowerblue", "gold")
+ggplot(my_amazing_data, aes(x = Sepal.Length, y = Sepal.Width, fill = Species)) + 
+  geom_point(shape = 21, size = 4) +
+  scale_fill_manual(values = my_colors) + scale_color_manual(values = my_colors) +
+  geom_smooth(formula = y~x, aes(color = Species), method = 'lm') +
+  scale_x_continuous(name = "Sepal length (cm)", limits = c(4,8)) +
+  scale_y_continuous(name = "Sepal width (cm)", limits = c(2,5)) +
+  facet_wrap(~Species) +
+  theme_dark()
